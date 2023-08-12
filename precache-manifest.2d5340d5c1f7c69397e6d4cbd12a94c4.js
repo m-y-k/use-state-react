752cac515d201b765e08f25f46f3ef671e74740a
Yusuf <mohammad.yusufkhan12345@gmail.com>
2023-08-12 17:28:45 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.2d5340d5c1f7c69397e6d4cbd12a94c4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "133386dc8dabe111bfa9",
+    "url": "//use-state-react/static/js/main.133386dc.chunk.js"
+  },
+  {
+    "revision": "1d670cbd77c3dc457119",
+    "url": "//use-state-react/static/js/1.1d670cbd.chunk.js"
+  },
+  {
+    "revision": "3e61c99de16c6f623ef9",
+    "url": "//use-state-react/static/js/runtime~main.3e61c99d.js"
+  },
+  {
+    "revision": "94d9ff86f3b7e4222bfe54eb88ece01d",
+    "url": "//use-state-react/index.html"
+  }
+];
